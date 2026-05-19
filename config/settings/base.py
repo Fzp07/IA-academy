6aa2ad6a1adab90a80cd5a1ab122493b2f74f6ff
Fzp07 <fzp07@github.com>
2026-05-19 14:00:18 -0500

    Agregar valor por defecto para SECRET_KEY en base.py para build

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-dev-key-not-for-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
