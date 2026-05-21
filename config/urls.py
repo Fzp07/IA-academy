41b6b7d38a0e61239fd47a2bb052b507120bbd19
Fzp07 <fzp07@users.noreply.github.com>
2026-05-21 14:54:14 -0500

    Fix: servir archivos media en producción (avatars)

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('genetic/', include('apps.genetic_demo.urls')),
 ]
 
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
 urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATICFILES_DIRS[0]})]
